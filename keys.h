2bddd2e55043835ad2c800293576f5c7a08c54bb
Daniel Medrano <bestenglishking@hotmail.com>
2016-01-27 09:43:16 -0600

    Add functions declaration

---
+++ keys.h
@@ -0,0 +1,8 @@
+#ifndef KEYS_H_INCLUDED
+#define KEYS_H_INCLUDED
+
+int syntax_check(const char *str);
+int matching_closing_key(int at);
+int matching_opening_key(int at);
+
+#endif // KEYS_H_INCLUDED
