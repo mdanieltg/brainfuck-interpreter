c3c3643fd7bd4b7e8c91aa1d2af606f8087efdcc
Daniel Medrano <bestenglishking@hotmail.com>
2016-01-27 11:15:39 -0600

    Update declarations

--- keys.h
+++ keys.h
@@ -1,8 +1,7 @@
 #ifndef KEYS_H_INCLUDED
 #define KEYS_H_INCLUDED
 
-int syntax_check(const char *str);
-int matching_closing_key(int at);
-int matching_opening_key(int at);
+int matching_closing_key(int);
+int matching_opening_key(int);
 
 #endif // KEYS_H_INCLUDED
