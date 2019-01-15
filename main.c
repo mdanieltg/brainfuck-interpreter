0890321553ab54c51cf2a1ee7918fbe450f4393f
Daniel Medrano <bestenglishking@hotmail.com>
2019-01-14 21:43:28 -0600

    Same

--- main.c
+++ main.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#include <stdlib.h>
-//#include "keys.h"
 #include "files.h"
+#include "keys.h"
 #include "syntax_check.h"
 
 #ifndef MAX_SIZE_ARRAY
@@ -13,97 +12,84 @@ int main(int argc, char* argv[])
 {
     if (argc != 2)
     {
-        puts("\n\tERROR: No se proporciono ruta."); // No file name was provided
+        // No file name was provided
+        printf("ERROR: No se proporcionó ruta.\n\n");
         return(21);
     }
 
-    char fileName[512] = "";
-    char source[MAX_SIZE_ARRAY] = { };      // Source code file
-    char array[MAX_SIZE_ARRAY] = { };       // Array to work with bytes
-    char *ptr = array;                      // Pointer to the Array
-    char car = 0;
-    long i = 0;
-    char output[MAX_SIZE_ARRAY] = { };
+    char filename[512] = { 0 };
+    char source[MAX_SIZE_ARRAY] = { 0 };
+    char workspace[MAX_SIZE_ARRAY] = { 0 };
+    char output[MAX_SIZE_ARRAY] = { 0 };
+
+    // Pointer to the Array
+    char* ptr = workspace;
 
     // Identify arguments
-    strcpy(fileName, argv[1]);
+    strcpy(filename, argv[1]);
 
-    if (strlen(fileName) == 0)            
-        puts("\n\tERROR: No se proporciono ruta."); // No file name was provided
+    if (strlen(filename) == 0)
+        // No file name was provided
+        printf("ERROR: No se proporcionó ruta.\n\n");
     else
-    {                                // File name was provided
-        readFile(fileName, source);         // Truncate contents of source code file into array source
+    {
+        // File name was provided
+        // Truncate contents of source code file into array source
+        read_file(filename, source);
 
         if (strlen(source) != 0)
-        {          // Array not empty
-            int synCode = 0;
-            synCode = syntax_check(source); // Store result of the syntax check
+        {
+            // Store result of the syntax check
+            int syntax_check_result = syntax_check(source);
 
-            if (synCode != 0)
-            {             // Syntax error
-                puts("\n\tERROR: Error de sintaxis.");
+            if (syntax_check_result != 0)
+            {
+                // Syntax error
+                printf("ERROR: Error de sintaxis.\n\n");
 
-                if (synCode < 0)
-                {          // Found single opening key
-                    printf("\t\tHay una llave sin cerrar en la posicion %d.\n\n", (-1 * synCode));
-                }
+                if (syntax_check_result < 0)
+                    // Found single opening key
+                    printf(">Hay una llave sin cerrar en la posicion %d.\n\n",
+                        (-1 * syntax_check_result));
                 else
-                {                    // Found single closing key
-                    printf("\t\tHay una llave sin abrir en la posicion %d.\n\n", synCode);
-                }
+                    // Found single closing key
+                    printf(">Hay una llave sin abrir en la posicion %d.\n\n",
+                        syntax_check_result);
             }
             else
-            {                        // No error found
+            {
+                // No error found
                 // Iterate through the contents of the source code
-                while (i < MAX_SIZE_ARRAY) {
-                    car = source[i];
+                for (long i = 0; i < MAX_SIZE_ARRAY; i++)
+                {
+                    char car = source[i];
 
-                    switch (car)
+                    if      (car == '+') ++*ptr;
+                    else if (car == '-') --*ptr;
+                    else if (car == '>') ++ptr;
+                    else if (car == '<') --ptr;
+                    else if (car == ',') *ptr = getchar();
+                    else if (car == '.') output[strlen(output)] = *ptr;
+                    else if (car == '[')
                     {
-                        case '+':
-                            ++*ptr;
-                            break;
-                        case '-':
-                            --*ptr;
-                            break;
-                        case '>':
-                            ++ptr;
-                            break;
-                        case '<':
-                            --ptr;
-                            break;
-                        case ',':
-                            *ptr = getchar();
-                            break;
-                        case '.':
-                            output[strlen(output)] = *ptr;
-                            break;
-                        case '[':
-                            if (*ptr == 0)
-                                i = matching_closing_key(i, source);
-                                
-                            break;
-                        case ']':
-                            if (*ptr != 0)
-                                i = matching_opening_key(i, source);
-                                
-                            break;
+                        if (*ptr == 0)
+                            i = matching_closing_key(i, source);
                     }
-
-                    i++;
+                    else if (car == ']')
+                    {
+                        if (*ptr != 0)
+                            i = matching_opening_key(i, source);
+                    }
                 }
 
-                putchar('\n');
-                putchar('\n');
-
-                printf("Resultado: %s\n", output);
+                printf("Resultado:\n");
+                printf("%s\n", output);
             }
         }
         else
-        {                            // No contents / file empty
-            puts("\n\tERROR: No se pudo leer el archivo o no tenia contenidos.");
-        }
+            // No contents / file empty
+            printf("ERROR: No se pudo leer el archivo o no tenía contenidos.\n\n");
     }
 
-    return 1;
+    return 0;
 }
